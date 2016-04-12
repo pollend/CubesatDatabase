071ab598b42dc5e8c12fa603ed5cdef9d2d69a25
Michael Pollind <mpollind@gmail.com>
2016-04-12 09:31:20 -0700

    started working on search bar

---
+++ resources/assets/typescript/dropdown-search.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input, Output, OnChanges, EventEmitter} from 'angular2/core';
+
+@Component({
+    selector: 'dropdown-search',
+    templateUrl: 'templates/dropdown-search.component.html'
+})
+
+
+export class DropdownSearchComponent<T>  { 
+	
+
+}
+
