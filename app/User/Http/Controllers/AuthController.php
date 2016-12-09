5448e6943d22a8b25ca5d4ec15776a97f361b13f
Michael Pollind <mpollind@gmail.com>
2016-12-09 09:38:44 -0800

    maded username unique

--- app/User/Http/Controllers/AuthController.php
+++ app/User/Http/Controllers/AuthController.php
@@ -61,7 +61,7 @@ class AuthController extends Controller
     public function postRegiser(Request $request)
     {
         $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
+        'name' => 'required|max:255|unique:users,username',
         'email' => 'required|email|max:255|unique:users',
         'password' => 'required|confirmed|min:6',
         ]);
